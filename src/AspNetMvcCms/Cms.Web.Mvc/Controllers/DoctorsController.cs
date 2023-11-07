8c621783bb4ff7a5ccb44244b113d7e10650d119
assli08 <assli.isildak@gmail.com>
2023-11-07 18:51:05 +0300

    Doctors controller and views added

---
+++ src/AspNetMvcCms/Cms.Web.Mvc/Controllers/DoctorsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cms.Web.Mvc.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
