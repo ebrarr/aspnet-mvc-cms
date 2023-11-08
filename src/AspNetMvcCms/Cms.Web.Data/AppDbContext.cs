a66fcf3e75ed680e69b80cfe2d460df760851ae6
ebraryanik <ebrarucar96@gmail.com>
2023-11-08 17:30:33 +0300

    Data katmanı oluşturuldu, AppDbContext-EntityBase classları eklendi

---
+++ src/AspNetMvcCms/Cms.Web.Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.Web.Data
+{
+    public class AppDbContext : DbContext
+    {
+    }
+}
